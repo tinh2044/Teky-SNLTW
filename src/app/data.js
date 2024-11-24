9d5f500b5b1bb19c9c0fa1ef60abbd175efe578a
Chi Tinh <tinhnguyen23122004@gmail.com>
2024-11-24 15:56:58 +0700

    Describe your changes here

--- src/app/data.js
+++ src/app/data.js
@@ -102,33 +102,7 @@ export const BtnList = [
   { label: "HP10", link: "/hp10", icon: "contact", newTab: false },
   { label: "HP11", link: "/hp11", icon: "contact", newTab: false },
   { label: "HP12", link: "/hp11", icon: "contact", newTab: false },
-  // { label: "About", link: "/about", icon: "about", newTab: false },
-  // { label: "Projects", link: "/projects", icon: "projects", newTab: false },
-  // { label: "Contact", link: "/contact", icon: "contact", newTab: false },
-  // {
-  //   label: "Github",
-  //   link: "https://www.github.com/codebucks27",
-  //   icon: "github",
-  //   newTab: true,
-  // },
-  // {
-  //   label: "LinkedIn",
-  //   link: "https://www.linkedin.com/in/codebucks",
-  //   icon: "linkedin",
-  //   newTab: true,
-  // },
-  // {
-  //   label: "X",
-  //   link: "https://www.x.com/code_bucks",
-  //   icon: "twitter",
-  //   newTab: true,
-  // },
-  // {
-  //   label: "Resume",
-  //   link: "/resume.pdf",
-  //   icon: "resume",
-  //   newTab: true,
-  // },
+
 ];
 
 
