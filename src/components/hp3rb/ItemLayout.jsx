ff98cc503f7591f35428dcadc203d41ee7ec93b5
Chi Tinh <tinhnguyen23122004@gmail.com>
2024-11-26 17:49:54 +0700

    fix bug

---
+++ src/components/hp3rb/ItemLayout.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { motion } from "framer-motion";
+import clsx from "clsx";
+
+const ItemLayout = ({ children, className }) => {
+    return (
+        <motion.div
+            initial={{ y: 50, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ once: true }}
+            className={clsx(
+                "custom-bg p-2 sm:p-2 rounded-xl flex items-center justify-center space-y-8 hover-bn",
+                className
+            )}
+        >
+            {children}
+        </motion.div>
+    );
+};
+
+export default ItemLayout;
