516c74079b9b6f1ba3c581218e23fafd3a2dd734
Chi Tinh <tinhnguyen23122004@gmail.com>
2024-11-26 11:32:14 +0700

    del

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <FireFliesBackground />
-        <Sound />
+        {/* <Sound /> */}
         <div id="my-modal" />
       </body>
     </html>
